34bd1a2eb2ffee919729ee2601828420bb83f92a
anita1218 <62133677+anita1218@users.noreply.github.com>
2020-04-30 22:05:38 +0800

    Update settings.py

--- 金融/st2-master/students/settings.py
+++ 金融/st2-master/students/settings.py
@@ -35,9 +35,9 @@ from linebot.models import *
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'Ub8e3cf75739079f25a50f82b2cbd4c63'
 # 必須放上自己的Channel Access Token
-LINE_CHANNEL_ACCESS_TOKEN ='528gEzSdPX8Pw9mTJlfpXRpTBdETmaT0LJuHs8+3/qBsiz1lWzBRIfWXQexgOdiPyGxeLtL9HAggCjAp0LBJcqKAdv7h8Ux5JQpKMioyobyYoBQpwer/eA3cA1e89/b80sl219M34lvpXlAAFud++wdB04t89/1O/w1cDnyilFU='
+LINE_CHANNEL_ACCESS_TOKEN ='mY79CNbS4kQwur/p8olVbIYlQVyfaxGw8uHtCGS/20Mz9sM4RafPyXRjiyQ1BB97e8mzeGZoh/kPhIVX51E+oQYpIcsGMBL6UTReb+eZi5OMdd/I+YwgYi1iNUjeRZIXA61RHE1wBkLvKKQmyuY29wdB04t89/1O/w1cDnyilFU='
 # 必須放上自己的Channel Secret
-LINE_CHANNEL_SECRET='43664ae683e1af5eb58087c85b74216d'
+LINE_CHANNEL_SECRET='f1649a55ecffddac0d43c69665d6e46c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = ['*']
